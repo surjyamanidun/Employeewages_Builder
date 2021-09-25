6d9629accd163d596d1facae568d225cb0c9e4fc
surjya <surjyamaniduan@gmail.com>
2021-09-25 22:50:16 +0530

    add:Employee wages

--- EmploeewagesBuilder.java
+++ EmploeewagesBuilder.java
@@ -1,5 +1,29 @@
 package employeewages_builder;
-
+import java.util.Random;
 public class EmploeewagesBuilder {
 
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee Wage Computation Program");
+        
+        int IS_FULLTIME = 1;
+        int Emp_Rate_PerHr = 20;
+        
+        int empHrs = 0;
+        int empWage = 0;
+        
+        Random random = new Random();
+        int empCheck = random.nextInt(2);
+        if (empCheck == IS_FULLTIME)
+            empHrs = 8;
+        else
+            empHrs = 0;
+        
+        empWage = Emp_Rate_PerHr * empHrs;
+        System.out.println("The daily wage of an employee is : " + empWage);
+    }
 }
