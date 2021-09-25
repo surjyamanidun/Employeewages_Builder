33e65a3642ac76e81fddce49ddd622037e8d1765
surjya <surjyamaniduan@gmail.com>
2021-09-25 22:55:12 +0530

    add: employee full or part time

--- EmploeewagesBuilder.java
+++ EmploeewagesBuilder.java
@@ -8,22 +8,26 @@ public class EmploeewagesBuilder {
 
 
     public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Computation Program");
-        
-        int IS_FULLTIME = 1;
-        int Emp_Rate_PerHr = 20;
-        
-        int empHrs = 0;
-        int empWage = 0;
-        
-        Random random = new Random();
-        int empCheck = random.nextInt(2);
-        if (empCheck == IS_FULLTIME)
-            empHrs = 8;
-        else
-            empHrs = 0;
-        
-        empWage = Emp_Rate_PerHr * empHrs;
-        System.out.println("The daily wage of an employee is : " + empWage);
-    }
-}
+    	 
+    	        System.out.println("Welcome To Employee Wage Computation Program");
+    	        
+    	        int IS_FULLTIME = 1;
+    	        int IS_PARTTIME = 2;
+    	        int Emp_Rate_PerHr = 20;
+    	        
+    	        int empHrs = 0;
+    	        int empWage = 0;
+    	        
+    	        Random random = new Random();
+    	        int empCheck = random.nextInt(3);
+    	        if (empCheck == IS_FULLTIME)
+    	            empHrs = 8;
+    	        else if (empCheck == IS_PARTTIME)
+    	            empHrs = 4;
+    	        else
+    	            empHrs = 0;
+    	        
+    	        empWage = Emp_Rate_PerHr * empHrs;
+    	        System.out.println("The daily wage of an employee is : " + empWage);
+    	    }
+    	}
