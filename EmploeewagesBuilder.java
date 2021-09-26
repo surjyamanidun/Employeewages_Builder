2b9fc714a1f7ca82e6124945245106e2c6fac1c2
surjya <surjyamaniduan@gmail.com>
2021-09-26 13:57:24 +0530

    add:calculate max hour

--- EmploeewagesBuilder.java
+++ EmploeewagesBuilder.java
@@ -2,10 +2,11 @@ package employeewages_builder;
 import java.util.Random;
 public class EmploeewagesBuilder {
 	
-	    public static final int IS_FULLTIME = 1;
+	 public static final int IS_FULLTIME = 1;
 	    public static final int IS_PARTTIME = 2;
 	    public static final int Emp_Rate_PerHr = 20;
 	    public static final int NO_WORKINGDAYS = 20;
+	    public static final int MAX_HRS_MONTH = 100;
 
 	    public static void main(String[] args) {
 	        System.out.println("Welcome To Employee Wage Computation Program");
@@ -13,8 +14,11 @@ public class EmploeewagesBuilder {
 	        int empHrs = 0;
 	        int empWage = 0;
 	        int totalEmpWage = 0;
-	       
-	        for (int day = 0; day < NO_WORKINGDAYS; day++) {
+	        int totalEmpHrs = 0;
+	        int totalWorkingDays = 0;
+	        
+	        while (totalEmpHrs <= MAX_HRS_MONTH && totalWorkingDays < NO_WORKINGDAYS) {
+	            totalWorkingDays++;
 	            Random random = new Random();
 	            int empCheck = random.nextInt(3);
 	            switch (empCheck) {
@@ -27,11 +31,12 @@ public class EmploeewagesBuilder {
 	                default: 
 	                    empHrs = 0;
 	            }
-	            
 	            empWage = Emp_Rate_PerHr * empHrs;
-	            totalEmpWage += empWage;
-	            System.out.println("The daily wage of an employee is : " + empWage);
+	            totalEmpHrs += empHrs;
+	            System.out.println("Day : " + totalWorkingDays + " : Employee worked : " + empHrs + " Hours " +
+	                               ", Employee Wage : " + empWage);
 	        }
-	        System.out.println("The total wage of an employee in a month is : " + totalEmpWage);
+	        totalEmpWage = totalEmpHrs * Emp_Rate_PerHr;
+	        System.out.println("Total Wages of an Employee is : " + totalEmpWage);
 	    }
 	}
